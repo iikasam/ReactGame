85c4bb615e7539189e46b6e62ecd6d96bf99d57e
iikasam <iikasam@naver.com>
2022-05-12 16:30:16 +0900

    React 웹 개발 시작하기 2강 8단원

--- dice_game/src/index.js
+++ dice_game/src/index.js
@@ -1,6 +1,12 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
+// render 메소드는 화면을 그린다는 뜻
 ReactDOM.render(
-  <h1>안녕 리액트!</h1>,
+  <>
+  <h1 id="title">가위바위보</h1>
+  <button className="hand">가위</button>
+  <button className="hand">바위</button>
+  <button className="hand">보</button>
+  </>,
   document.getElementById('root')
 );
